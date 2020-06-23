97c470999ee9ea9b6ff8fb65c0ceb15425a4c273
GKAtwood <gabrielle.k85@gmail.com>
2020-06-22 21:46:53 -0600

    sdfs

---
+++ src/Components/FileTwo.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default function FileTwo(){
+    return(
+        <div className='card'>Card
+        
+        
+        
+        
+        </div>
+    )
+}
